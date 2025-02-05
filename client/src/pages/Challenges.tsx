28755841b68e3824bd0041c4b57be2123198cd5b
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2025-02-05 10:52:45 +0000

    Challenges section design completed

--- client/src/pages/Challenges.tsx
+++ client/src/pages/Challenges.tsx
@@ -1,3 +1,54 @@
+import plantIcon from '/images/icons/plant.svg';
+import handLeafIcon from '/images/icons/handleaf.svg';
+import flowerBranchIcon from '/images/icons/flowerbranch.svg';
+import './Challenges.css';
+
 export function Challenges() {
-  return <h1 style={{ color: 'white' }}>Challenges</h1>;
+  return (
+    <>
+      <div className="challenge-container">
+        <div className="challenge-col gap-4">
+          <div className="challenge-row">
+            <h1 className="text-3xl">Challenges</h1>
+          </div>
+
+          <div className="challenge-row justify-center italic">
+            Grow your wellness plant today!
+          </div>
+
+          <div className="challenge-col items-center">
+            <ul className="challenges">
+              <li className="challenge-box">
+                <div>
+                  <h3 className="text-l">Daily Challenge</h3>
+                  <p className="text-sm text-gray-600">0/3 Completed</p>
+                </div>
+                <div>
+                  <img src={plantIcon} alt="daily-icon" />
+                </div>
+              </li>
+              <li className="challenge-box">
+                <div>
+                  <h3 className="text-l">Weekly Challenge</h3>
+                  <p className="text-sm text-gray-600">0/3 Completed</p>
+                </div>
+                <div>
+                  <img src={handLeafIcon} alt="weekly-icon" />
+                </div>
+              </li>
+              <li className="challenge-box">
+                <div>
+                  <h3 className="text-l">Monthly Challenge</h3>
+                  <p className="text-sm text-gray-600">0/3 Completed</p>
+                </div>
+                <div>
+                  <img src={flowerBranchIcon} alt="monthly-icon" />
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
