f71e6eecd970e3beb40e9a37ff737035449dc455
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2025-02-06 00:47:08 +0000

    Changes list into button elements, add comments

--- client/src/pages/Challenges.tsx
+++ client/src/pages/Challenges.tsx
@@ -8,17 +8,21 @@ export function Challenges() {
     <>
       <div className="challenge-container">
         <div className="challenge-col gap-4">
+          {/* Title */}
           <div className="challenge-row">
             <h1 className="text-3xl">Challenges</h1>
           </div>
 
+          {/* Motivation */}
           <div className="challenge-row justify-center italic">
             Grow your wellness plant today!
           </div>
 
+          {/* Challenges Category */}
           <div className="challenge-col items-center">
-            <ul className="challenges">
-              <li className="challenge-box">
+            <div className="challenges">
+              {/* Daily */}
+              <button className="challenge-box">
                 <div>
                   <h3 className="text-l">Daily Challenge</h3>
                   <p className="text-sm text-gray-600">0/3 Completed</p>
@@ -26,8 +30,10 @@ export function Challenges() {
                 <div>
                   <img src={plantIcon} alt="daily-icon" />
                 </div>
-              </li>
-              <li className="challenge-box">
+              </button>
+
+              {/* Weekly */}
+              <button className="challenge-box">
                 <div>
                   <h3 className="text-l">Weekly Challenge</h3>
                   <p className="text-sm text-gray-600">0/3 Completed</p>
@@ -35,8 +41,10 @@ export function Challenges() {
                 <div>
                   <img src={handLeafIcon} alt="weekly-icon" />
                 </div>
-              </li>
-              <li className="challenge-box">
+              </button>
+
+              {/* Monthly */}
+              <button className="challenge-box">
                 <div>
                   <h3 className="text-l">Monthly Challenge</h3>
                   <p className="text-sm text-gray-600">0/3 Completed</p>
@@ -44,8 +52,8 @@ export function Challenges() {
                 <div>
                   <img src={flowerBranchIcon} alt="monthly-icon" />
                 </div>
-              </li>
-            </ul>
+              </button>
+            </div>
           </div>
         </div>
       </div>
