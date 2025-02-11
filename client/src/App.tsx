9db5d30d3e3ca50377f783f97c41b6660b1cb9c7
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2025-02-11 03:26:56 +0000

    Add design for time challenge

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { NavBar } from './components/NavBar';
 import { Dashboard } from './pages/Dashboard';
 import { Challenges } from './pages/Challenges';
 import { Profile } from './pages/Profile';
-import { TimeChallenge } from './components/TimeChallenge';
+import { TimeChallenge } from './pages/TimeChallenge';
 
 export default function App() {
   return (
@@ -11,9 +11,9 @@ export default function App() {
       <Route path="/" element={<NavBar />}>
         <Route index element={<Dashboard />} />
         <Route path="/challenges" element={<Challenges />} />
-        <Route path="/time-challenge" element={<TimeChallenge />} />
         <Route path="/profile" element={<Profile />} />
       </Route>
+      <Route path="/time-challenge" element={<TimeChallenge />} />
     </Routes>
   );
 }
---
+++ client/src/pages/TimeChallenge.tsx
@@ -0,0 +1,44 @@
+import { useNavigate } from 'react-router-dom';
+import { TiChevronLeft } from 'react-icons/ti';
+import { GoDotFill } from 'react-icons/go';
+import { FaPauseCircle, FaPlay } from 'react-icons/fa';
+
+import logo from '/images/icons/logo.svg';
+import './TimeChallenge.css';
+
+export function TimeChallenge() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="time-container">
+      <div className="time-col">
+        <div className="time-row">
+          <button className="text-3xl" onClick={() => navigate('/challenges')}>
+            <TiChevronLeft />
+          </button>
+        </div>
+        <div className="time-col justify-between items-center mt-6">
+          {/* Leaf Logo */}
+          <div className="leaf">
+            <img src={logo} alt="logo" />
+          </div>
+
+          <div className="time-col justify-center items-center gap-12">
+            <p className="text-5xl">4</p>
+            <div className="pause-play-buttons">
+              <FaPauseCircle className="text-5xl" />
+              <FaPlay className="text-5xl" />
+            </div>
+
+            {/* Dots to be transformed into mini logo */}
+            <div className="time-row justify-center items-center text-3xl">
+              <GoDotFill />
+              <GoDotFill />
+              <GoDotFill />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
