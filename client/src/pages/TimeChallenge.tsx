08407b18d22b37800a9d785fbfe4a17597850598
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2025-02-12 02:51:13 +0000

    Added functionality to the timer countdown with instruction iteration

--- client/src/pages/TimeChallenge.tsx
+++ client/src/pages/TimeChallenge.tsx
@@ -5,9 +5,39 @@ import { FaPauseCircle, FaPlay } from 'react-icons/fa';
 
 import logo from '/images/icons/logo.svg';
 import './TimeChallenge.css';
+import { useState } from 'react';
 
 export function TimeChallenge() {
   const navigate = useNavigate();
+  const [timer, setTimer] = useState(4);
+  const [index, setIndex] = useState(0);
+  const [toggle, setToggle] = useState(false);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | number>();
+  const instructions = ['Inhale', 'Hold', 'Exhale'];
+
+  function handleClick() {
+    if (!toggle) {
+      const id = setInterval(() => {
+        setTimer((prev) => {
+          if (prev > 0) {
+            return prev - 1; // Decrement
+          } else {
+            // Circle around the array
+            setIndex((prevIndex) =>
+              prevIndex >= instructions.length - 1 ? 0 : prevIndex + 1
+            );
+            return 4; // Reset the timer for the next round.
+          }
+        });
+      }, 1000);
+      setIntervalId(id);
+    } else {
+      clearInterval(intervalId);
+    }
+
+    // Play or Pause
+    setToggle((prevToggle) => !prevToggle);
+  }
 
   return (
     <div className="time-container">
@@ -24,11 +54,17 @@ export function TimeChallenge() {
           </div>
 
           <div className="time-col justify-center items-center gap-12">
-            <p className="text-5xl">4</p>
-            <p className="text-base italic">Inhale, Hold, Exhale</p>
+            <p className="text-5xl">{timer}</p>
+            <p className="text-base italic">{instructions[index]}</p>
             <div className="pause-play-buttons">
-              <FaPauseCircle className="text-5xl" />
-              <FaPlay className="text-5xl hidden" />
+              <FaPauseCircle
+                className={`text-5xl ${toggle ? '' : 'hidden'}`}
+                onClick={handleClick}
+              />
+              <FaPlay
+                className={`text-5xl ${toggle ? 'hidden' : ''}`}
+                onClick={handleClick}
+              />
             </div>
 
             {/* Dots to be transformed into mini logo */}
